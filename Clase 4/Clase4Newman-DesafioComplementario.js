e527415bb931922e0c02a4ca2284d719a78ebdff
Samantha Newman <newmansamantha00@gmail.com>
2021-11-05 13:48:24 -0300

    clase 4

---
+++ Clase 4/Clase4Newman-DesafioComplementario.js
@@ -0,0 +1,7 @@
+alert("A continuación calcularemos tu sueldo neto:")
+
+let SueldoBruto = prompt("ingresá tu sueldo bruto:")
+
+let SueldoNeto = SueldoBruto -  ((17 * SueldoBruto ) / 100);
+
+alert("Tu sueldo neto es: " + SueldoNeto)
