25d521c4f7ef13265cc8545ba2cb9d02912b46f9
Samantha Newman <newmansamantha00@gmail.com>
2021-11-05 13:30:20 -0300

    clase 4

---
+++ Clase 4/Clase4Newman.js
@@ -0,0 +1,7 @@
+let CotizacionDolarBlue = 195;
+
+var PrecioenUSD = prompt("Ingresá el precio de tu producto en dólares");
+
+let PrecioenAR = PrecioenUSD*195;
+
+alert("Tu precio en pesos argentinos es: " + PrecioenAR);
