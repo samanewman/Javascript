ceacd1761d671a314d4cac0d47c2a180798abf30
Samantha Newman <newmansamantha00@gmail.com>
2021-11-24 22:56:56 -0300

    clase 7

--- ConversorDivisas.js
+++ ConversorDivisas.js
@@ -13,15 +13,21 @@ const PrecioProductos = {bomba: 510, kitbateria: 340, panelsolar: 260};
 
 const CotizacionDolar = {blue: 200, oficial: 106, Bolsa: 184, liqui: 184, solidario: 174};
 
-let PrecioenBlueBomba= PrecioProductos.bomba*CotizacionDolar.blue;
-let PrecioenOficialBomba= PrecioProductos.bomba*CotizacionDolar.oficial;
-
-console.log(PrecioProductos.bomba);
-console.log(PrecioenBlueBomba);
-
 let div = document.createElement("div");
-div.innerHTML = "<div>Precio en USD: {$PrecioProductos.bomba}<br>Precio en Pesos: {$PrecioBlueBomba}</div>";
+div.innerHTML = `<div>Precio en USD: $ ${PrecioProductos.bomba}`;
 let contenedorbomba = document.getElementById("contenedorbomba");
 contenedorbomba.appendChild(div);
+
+function calcularprecio(){
+
+    let PrecioenBlueBomba= PrecioProductos.bomba*CotizacionDolar.blue;
+    let PrecioenOficialBomba= PrecioProductos.bomba*CotizacionDolar.oficial;
+
+    let div = document.createElement("div");
+    div.innerHTML = `Precio en Pesos: $ ${PrecioenBlueBomba}</div>`;
+    let contenedorbomba = document.getElementById("contenedorbomba");
+    contenedorbomba.appendChild(div);
+
+}
 
 
