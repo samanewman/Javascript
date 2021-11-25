59296d1d5b9648b2bc977c1a46948834f3c60bf1
Samantha Newman <newmansamantha00@gmail.com>
2021-11-24 22:07:33 -0300

    New Branch

--- Proyecto-Final/ConversorDivisas.js
+++ Proyecto-Final/ConversorDivisas.js
@@ -1,4 +1,4 @@
-const CotizacionDolar = {blue: 200, oficial: 106, Bolsa: 184, liqui: 184, solidario: 174};
+/*const CotizacionDolar = {blue: 200, oficial: 106, Bolsa: 184, liqui: 184, solidario: 174};
 
 var PrecioenUSD = prompt("Ingresá el precio de tu producto en dólares");
 
@@ -6,4 +6,22 @@ let PrecioenBlue= PrecioenUSD*CotizacionDolar.blue;
 let PrecioenOficial= PrecioenUSD*CotizacionDolar.oficial;
 
 alert("Tu precio en pesos argentinos es: " + PrecioenBlue);
-alert("Tu precio con cotización al dólar oficial en pesos argentinos es: " + PrecioenOficial);
+alert("Tu precio con cotización al dólar oficial en pesos argentinos es: " + PrecioenOficial);*/
+
+
+const PrecioProductos = {bomba: 510, kitbateria: 340, panelsolar: 260};
+
+const CotizacionDolar = {blue: 200, oficial: 106, Bolsa: 184, liqui: 184, solidario: 174};
+
+let PrecioenBlueBomba= PrecioProductos.bomba*CotizacionDolar.blue;
+let PrecioenOficialBomba= PrecioProductos.bomba*CotizacionDolar.oficial;
+
+console.log(PrecioProductos.bomba);
+console.log(PrecioenBlueBomba);
+
+let div = document.createElement("div");
+div.innerHTML = "<div>Precio en USD: {$PrecioProductos.bomba}<br>Precio en Pesos: {$PrecioBlueBomba}</div>";
+let contenedorbomba = document.getElementById("contenedorbomba");
+contenedorbomba.appendChild(div);
+
+
