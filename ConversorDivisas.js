a4152f9e331f95763cf9bd5ad547d5c41d845457
Samantha Newman <newmansamantha00@gmail.com>
2021-12-24 15:48:25 -0300

    jquery +toggler

--- ConversorDivisas.js
+++ ConversorDivisas.js
@@ -3,90 +3,40 @@ const PrecioProductosusd = {bomba: 510, kitbateria: 340, panelsolar: 260};
 
 const CotizacionDolar = {blue: 200, oficial: 106, Bolsa: 184, liqui: 184, solidario: 174};
 
-/*let contenedorbomba = document.getElementById("contenedorbombausd");
-contenedorbomba.innerHTML = `<div>Precio en USD: $ ${PrecioProductosusd.bomba}`;
 
-let contenedorkit = document.getElementById("contenedorkitusd");
-contenedorkit.innerHTML = `<div>Precio en USD: $ ${PrecioProductosusd.kitbateria}`;
+$("#contenedorbombausd").append(`Precio en USD: $ ${PrecioProductosusd.bomba}`);
 
-let contenedorpanel = document.getElementById("contenedorpanelusd");
-contenedorpanel.innerHTML = `<div>Precio en USD: $ ${PrecioProductosusd.panelsolar}`;
+$("#contenedorkitusd").append(`Precio en USD: $ ${PrecioProductosusd.kitbateria}`);
+
+$("#contenedorpanelusd").append(`Precio en USD: $ ${PrecioProductosusd.panelsolar}`);
 
 
+PrecioenBlueBomba = PrecioProductosusd.bomba*CotizacionDolar.blue
+$("#contenedorbomba").append(`Precio en Pesos: $ ${PrecioenBlueBomba}`);
 
-let botonbomba = document.getElementById("calcularpreciobomba");
-botonbomba.addEventListener("click", calcularpreciobomba);
+PrecioenBlueKit = PrecioProductosusd.kitbateria*CotizacionDolar.blue
+$("#contenedorkit").append(`Precio en Pesos: $ ${PrecioenBlueKit}`);
 
-let botonkit = document.getElementById("calcularpreciokit");
-botonkit.addEventListener("click", calcularpreciokit);
-
-let botonpanel = document.getElementById("calcularpreciopanel");
-botonpanel.addEventListener("click", calcularpreciopanel);
-
-function calcularpreciobomba(){
-    
-    PrecioenBlueBomba = PrecioProductosusd.bomba*CotizacionDolar.blue
-  
-
-    let contenedorbomba = document.getElementById("contenedorbomba");
-    contenedorbomba.innerHTML = `Precio en Pesos: $ ${PrecioenBlueBomba}</div>`;
-
-}
-
-function calcularpreciokit(){
-    
-    PrecioenBlueKit = PrecioProductosusd.kitbateria*CotizacionDolar.blue
-
-    let contenedorkit = document.getElementById("contenedorkit");
-    contenedorkit.innerHTML = `Precio en Pesos: $ ${PrecioenBlueKit}</div>`;
-}
-
-
-
-function calcularpreciopanel(){
-    
-    PrecioenBluePanel = PrecioProductosusd.panelsolar*CotizacionDolar.blue
-    
-    let contenedorpanel = document.getElementById("contenedorpanel");
-    contenedorpanel.innerHTML = `Precio en Pesos: $ ${PrecioenBluePanel}</div>`;
-
-}
-
-*/
-
-$("#contenedorbombausd").append(`<div>Precio en USD: $ ${PrecioProductosusd.bomba}`);
-
-$("#contenedorkitusd").append(`<div>Precio en USD: $ ${PrecioProductosusd.kitbateria}`);
-
-$("#contenedorpanelusd").append(`<div>Precio en USD: $ ${PrecioProductosusd.panelsolar}`);
-
-
-
+PrecioenBluePanel = PrecioProductosusd.panelsolar*CotizacionDolar.blue
+$("#contenedorpanel").append(`Precio en Pesos: $ ${PrecioenBluePanel}`);
 
 
 $("#calcularpreciobomba").click(function(){
     
-    PrecioenBlueBomba = PrecioProductosusd.bomba*CotizacionDolar.blue
-  
-
-    $("#contenedorbomba").append(`Precio en Pesos: $ ${PrecioenBlueBomba}</div>`);
+    $("#contenedorbomba").toggle(1000);
 
 })
 
 $("#calcularpreciokit").click(function(){
     
-    PrecioenBlueKit = PrecioProductosusd.kitbateria*CotizacionDolar.blue
+    $("#contenedorkit").toggle(1000);
 
-    $("#contenedorkit").append(`Precio en Pesos: $ ${PrecioenBlueKit}</div>`);
 })
-
 
 
 $("#calcularpreciopanel").click(function(){
     
-    PrecioenBluePanel = PrecioProductosusd.panelsolar*CotizacionDolar.blue
-    
-     $("#contenedorpanel").append(`Precio en Pesos: $ ${PrecioenBluePanel}</div>`);
+    $("#contenedorpanel").toggle(1000);
 
 })
 
